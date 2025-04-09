fe4b049a621b4fef55d337019885101d4051b08b
John Gill <144708554+jagill12@users.noreply.github.com>
2025-04-08 21:03:34 -0400

    Create Iterating_Dictionary.py

---
+++ Learning_Python_Scripts/Iterating_Dictionary.py
@@ -0,0 +1,5 @@
+def main():
+    dictionary = {"Jake":"Football", "Greg":"Tennis", "Frederick":"Water Polo"}
+    for name, sport in dictionary.items():
+        print(name + " played " + sport + " in high school.")
+main()
