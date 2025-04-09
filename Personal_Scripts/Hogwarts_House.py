1eefd4c1c0658f8664f830043ed94401a7bea785
John Gill <144708554+jagill12@users.noreply.github.com>
2025-04-08 20:52:58 -0400

    Create Hogwarts_House.py

---
+++ Personal_Scripts/Hogwarts_House.py
@@ -0,0 +1,280 @@
+def Question1():
+
+    answer1 = str(input("Question 1: Say your friend is feeling distraught about a failed crush. How do you console them? Do you: " +"\n"
+                        "A: Tell them that life is to be lived from within, and not place your worth or happiness in another;." + "\n"
+                        "B: Remind them that love is a chemical reaction that's statistically guaranteed with healthy emotional processing." + "\n"
+                        "C: Say that this too shall pass, and you'll always be there for them no matter their struggle." + "\n"
+                        "D: Tell them these woes over unrequited love are meaningless distractions meant to make you just another failure."+ "\n"
+                        )).lower()
+
+    while answer1 != "a" and answer1 != "b" and answer1 != "c" and answer1 != "d":
+        answer1 = str(input("That's an invalid entry! Try again, either 'a', 'b', 'c' or 'd.'"))     
+
+    if answer1 == "a":
+        return 4
+    elif answer1 == "b":
+        return 3
+    elif answer1 == "c":
+        return 2
+    elif answer1 == "d":
+        return 1
+
+def Question2():
+    answer2 = str(input("Question 2: It's your last class of the week and you're excited for the big game tonight. Your professor assigns a take home quiz" + "\n"
+                        "Due Sunday night with a one-page synopsis on a 15 page reading. Your friend invites you to come over in 3 hours, which will" + "\n"
+                        "be 2 hours before the game starts. What do you do?" + "\n"
+                        "A: Say 'Why wait? It's the weekend baby! Let's fucking party!" + "\n"
+                        "B: Agree, but show up late because you made sure to finish all the homework without procrastinating." + "\n"
+                        "C: Agree, then finish the take-home quiz and save the reading for later, since you don't want to be late." + "\n"
+                        "D: Go straight home and miss the pregame to work on your little projects, then meet them right when the game starts."+ "\n"
+                        )).lower()
+
+    while answer2 != "a" and answer2 != "b" and answer2 != "c" and answer2 != "d":
+        answer2 = str(input("That's an invalid entry! Try again, either 'a', 'b', 'c' or 'd.'"))
+
+    if answer2 == "a":
+        return 4
+    elif answer2 == "b":
+        return 3
+    elif answer2 == "c":
+        return 2
+    elif answer2 == "d":
+        return 1
+
+def Question3():
+
+    answer3 = str(input("Question 3: You just found hard evidence that someone you hate cheated on a final that you studied hard for. How do you expose them?" + "\n"
+                        "A: Confront them and say if they don't tell, you will." + "\n"
+                        "B: Take it directly to the professor and let the system handle it." + "\n"
+                        "C: Confront them, scold them, do what you can to ensure they regret it, then tell the professor immediately." + "\n"
+                        "D: Blackmail them for what you can get before you tell the professor anyway."+ "\n"
+                        )).lower()
+    
+                        
+    while answer3 != "a" and answer3 != "b" and answer3 != "c" and answer3 != "d":
+        answer3 = str(input("That's an invalid entry! Try again, either 'a', 'b', 'c' or 'd.'")) 
+
+    if answer3 == "a":
+        return 4
+    elif answer3 == "b":
+        return 3
+    elif answer3 == "c":
+        return 2
+    elif answer3 == "d":
+        return 1
+
+def Question4():
+
+    answer4 = str(input("Question 4: What are your thoughts on art?" + "\n"
+                        "A: It's kind of whatever I guess." + "\n"
+                        "B: Love it. Beautiful expression of man. To be treasured and preserved." + "\n"
+                        "C: I have some styles I like." + "\n"
+                        "D: Useless outside of therapy." + "\n"
+                        )).lower()
+    
+                        
+    while answer4 != "a" and answer4 != "b" and answer4 != "c" and answer4 != "d":
+        answer4 = str(input("That's an invalid entry! Try again, either 'a', 'b', 'c' or 'd.'")) 
+        
+    if answer4 == "a":
+        return 4
+    elif answer4 == "b":
+        return 3
+    elif answer4 == "c":
+        return 2
+    elif answer4 == "d":
+        return 1
+
+def Question5():
+
+    answer5 = str(input("Question 5: You have a free weekend with no homework. What do you do?" + "\n"
+                        "A: Hangout with my many friends and have a great time together." + "\n"
+                        "B: Read, game, write, draw, build, experiment, and enjoy the nice weather. Perhaps collab with some pals." + "\n"
+                        "C: Go exploring nature and spend time with my few very close friends." + "\n"
+                        "D: Hone my skills and maintain my dominance." + "\n"
+                        )).lower()
+    
+                        
+    while answer5 != "a" and answer5 != "b" and answer5 != "c" and answer5 != "d":
+        answer5 = str(input("That's an invalid entry! Try again, either 'a', 'b', 'c' or 'd.'")) 
+        
+    if answer5 == "a":
+        return 4
+    elif answer5 == "b":
+        return 3
+    elif answer5 == "c":
+        return 2
+    elif answer5 == "d":
+        return 1
+
+def Question6():
+
+    answer6 = str(input("Question 6: What is your ideal romantic relationship?" + "\n"
+                        "A: We are a team." + "\n"
+                        "B: We are a good match." + "\n"
+                        "C: We are loyal." + "\n"
+                        "D: We are iconic." + "\n"
+                        )).lower()
+    
+                        
+    while answer6 != "a" and answer6 != "b" and answer6 != "c" and answer6 != "d":
+        answer6 = str(input("That's an invalid entry! Try again, either 'a', 'b', 'c' or 'd.'")) 
+        
+    if answer6 == "a":
+        return 4
+    elif answer6 == "b":
+        return 3
+    elif answer6 == "c":
+        return 2
+    elif answer6 == "d":
+        return 1
+
+def Question7():
+
+    answer7 = str(input("Question 7: What do you and your best friend do for fun?" + "\n"
+                        "A: Sports and outdoors. Maybe the pub." + "\n"
+                        "B: Indoor games and conversation." + "\n"
+                        "C: Art, nature, coffee, and Sunday brunch/margs." + "\n"
+                        "D: Business. The grind never stops." + "\n"
+                        )).lower()
+    
+                        
+    while answer7 != "a" and answer7 != "b" and answer7 != "c" and answer7 != "d":
+        answer7 = str(input("That's an invalid entry! Try again, either 'a', 'b', 'c' or 'd.'")) 
+        
+    if answer7 == "a":
+        return 4
+    elif answer7 == "b":
+        return 3
+    elif answer7 == "c":
+        return 2
+    elif answer7 == "d":
+        return 1
+
+def Question8():
+
+    answer8 = str(input("Question 8: What's your favorite time of day?" + "\n"
+                        "A: Noon-afternoon: love the vibrance of day." + "\n"
+                        "B: Night: I like being the only person awake when it's quiet." + "\n"
+                        "C: Evening: The calm silence helps me relax and concentrate. " + "\n"
+                        "D: Morning: I like to get a jumpstart on my day and be ahead of the curve." + "\n"
+                        )).lower()
+    
+                        
+    while answer8 != "a" and answer8 != "b" and answer8 != "c" and answer8 != "d":
+        answer8 = str(input("That's an invalid entry! Try again, either 'a', 'b', 'c' or 'd.'")) 
+        
+    if answer8 == "a":
+        return 4
+    elif answer8 == "b":
+        return 3
+    elif answer8 == "c":
+        return 2
+    elif answer8 == "d":
+        return 1
+
+def Question9():
+
+    answer9 = str(input("Question 9: What is your dream job?" + "\n"
+                        "A: I simply do not dream of labor." + "\n"
+                        "B: An important figurehead in my field of interest." + "\n"
+                        "C: Profiting off my passion without losing love for it." + "\n"
+                        "D: I will be a CEO." + "\n"
+                        )).lower()
+    
+                        
+    while answer9 != "a" and answer9 != "b" and answer9 != "c" and answer9 != "d":
+        answer9 = str(input("That's an invalid entry! Try again, either 'a', 'b', 'c' or 'd.'")) 
+        
+    if answer9 == "a":
+        return 4
+    elif answer9 == "b":
+        return 3
+    elif answer9 == "c":
+        return 2
+    elif answer9 == "d":
+        return 1
+
+def Question10():
+
+    answer10 = str(input("Question 10: And finally, which house do you want to be a part of?" + "\n"
+                        "A: Griffyndor." + "\n"
+                        "B: Ravenclaw." + "\n"
+                        "C: Hufflepuff." + "\n"
+                        "D: Slytherin." + "\n"
+                        )).lower()
+    
+                        
+    while answer10 != "a" and answer10 != "b" and answer10 != "c" and answer10 != "d":
+        answer10 = str(input("That's an invalid entry! Try again, either 'a', 'b', 'c' or 'd.'")) 
+        
+    if answer10 == "a":
+        return 4
+    elif answer10 == "b":
+        return 3
+    elif answer10 == "c":
+        return 2
+    elif answer10 == "d":
+        return 1   
+
+def answers(list):
+    index = 0
+    a = 0
+    b = 0
+    c = 0
+
+    d = 0
+    while index < len(list):
+        if list[index] == 4:
+            a += 1
+        elif list[index] == 3:
+            b += 1
+        elif list[index] == 2:
+            c += 1
+        else:
+            d += 1
+        index += 1
+    if a > b and a > c and a > d:
+        print("Griffindor!")
+    elif b > a and b > c and b > d:
+        print("Ravenclaw!")
+    elif c > a and c > b and c > d:
+        print("Hufflepuff!")
+    elif d > a and d > b and d > c:
+        print("Slytherin!")
+    elif a == b and a > c and a > d:
+        print("Griffindor or Ravenclaw!")
+    elif a == c and a > b and a > d:
+        print("Griffindor or Hufflepuff!")
+    elif a == d and a > b and a > c:
+        print("Griffindor or Slytherin!")
+    elif b == c and b > a and b > d:
+        print("Ravenclaw or Hufflepuff!")
+    elif b == d and b > a and b > c:
+        print("Ravenclaw or Slytherin!")
+    elif c == d and c > a and c > b:
+        print("Hufflepuff or Slytherin!")
+    
+
+
+def main():
+    print("Which Hogwarts House Are You")
+    print("Hello! Today we'll be playing a game set to determine which Hogwarts house you belong to." + "\n"
+        "The way it works, is I'll ask you 10 questions, to which you'll respond 'A', 'B', 'C', or 'D.'" + "\n"
+        "Then, I'll add up the total scores and tell you where you belong. Sound fun?")
+    a = Question1()
+    b = Question2()
+    c = Question3()
+    d = Question4()
+    e = Question5()
+    f = Question6()
+    g = Question7()
+    h = Question8()
+    i = Question9()
+    j = Question10()
+    numbers = []
+    numbers.extend([a, b, c, d, e, f, g, h, i ,j])
+    answers(numbers)
+    print(numbers)
+    
+main()
