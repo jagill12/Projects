c89ea7a7882f45ff3839c143dcbf7fa78f2eb660
John Gill <144708554+jagill12@users.noreply.github.com>
2025-09-08 11:37:43 -0400

    Delete Learning_Python_Scripts directory

--- Learning_Python_Scripts/Iterating_Dictionary.py
+++
@@ -1,5 +0,0 @@
-def main():
-    dictionary = {"Jake":"Football", "Greg":"Tennis", "Frederick":"Water Polo"}
-    for name, sport in dictionary.items():
-        print(name + " played " + sport + " in high school.")
-main()
--- Learning_Python_Scripts/Quadratic.py
+++
@@ -1,7 +0,0 @@
-def Formula(a, b, c):
-    '''Function: run three numbers through the quadratic formula
-       Parameters: 3 floats
-       Returns: 2 floats, the upper and lower limit'''
-    x = (-b + ((b ** 2) - (4 * (a * c))) ** (1/2)) / (2 * a)
-    y = (-b - (b ** 2 - (4 * (a * c))) ** (1/2)) / (2 * a)
-    return x, y
